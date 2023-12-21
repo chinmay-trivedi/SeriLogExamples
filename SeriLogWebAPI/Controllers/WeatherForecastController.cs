925ed8d3a4d0d14843bcf8248faba58d6485edcd
chinmay-trivedi <chinmay.trivedi@algoma.com>
2023-12-21 09:47:02 -0500

    Added Code for overriding only one class logging in SeriLogWebAPI project

--- SeriLogWebAPI/Controllers/WeatherForecastController.cs
+++ SeriLogWebAPI/Controllers/WeatherForecastController.cs
@@ -1,5 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
-
+using Serilog;
 namespace SeriLogWebAPI.Controllers
 {
     [ApiController]
@@ -12,10 +12,11 @@ namespace SeriLogWebAPI.Controllers
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
+        private readonly Serilog.ILogger _myLogger = Log.ForContext<WeatherForecastController>();
 
         public WeatherForecastController(ILogger<WeatherForecastController> logger)
         {
-            _logger = logger;
+            _logger = logger;            
             _logger.LogInformation("WeatherForecast controller called ");
         }
 
@@ -26,6 +27,13 @@ namespace SeriLogWebAPI.Controllers
             _logger.LogDebug("WeatherForecast get method Starting.");
             _logger.LogWarning("WeatherForecast get method Starting.");
             _logger.LogCritical("WeatherForecast get method Starting.");
+            var myClassLogger = _myLogger.ForContext("SourceContext", "MyClass");
+            myClassLogger.Information("MyClass Logging is enabled !!!");
+            myClassLogger.Debug("MyClass Debug Logging is enabled !!!");
+            myClassLogger.Error("MyClass Error Logging is enabled !!!");
+            myClassLogger.Fatal("MyClass Fatal Logging is enabled !!!");
+
+
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
